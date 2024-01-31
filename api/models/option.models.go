9f07622c1571cb2ee1f79019291b913bd51e7975
unknown <111202214715@mhs.dinus.ac.id>
2024-01-31 22:31:12 +0700

    option, transaction repository and define option model logic

--- api/models/option.models.go
+++ api/models/option.models.go
@@ -1,6 +1,10 @@
 package models
 
 import (
+	"errors"
+	"slices"
+	"strings"
+
 	"gorm.io/gorm"
 )
 
@@ -12,4 +16,13 @@ type Option struct {
 	Color      string `gorm:"not null;size:30;default:white"`
 	QuestionID uint
 	Question   Question `gorm:"constraint:OnDelete:CASCADE;"`
+}
+
+func (opt *Option) BeforeCreate(tx *gorm.DB) (err error) {
+	s := []string{"A", "B", "C", "D", "E"}
+	if !slices.Contains(s, strings.ToUpper(opt.Alphabet)) {
+		err := errors.New("Alphabet column: " + strings.ToUpper(opt.Alphabet) + " must be in " + strings.Join(s, ","))
+		return err
+	}
+	return
 }
--- api/repository/transaction.repository.go
+++ api/repository/transaction.repository.go
@@ -1,1 +1,36 @@
 package repository
+
+import (
+	"BagasA11/GSC-quizHealthEdu-BE/api/models"
+	"BagasA11/GSC-quizHealthEdu-BE/configs"
+
+	"gorm.io/gorm"
+)
+
+type TransRepository struct {
+	Db *gorm.DB
+}
+
+func NewTransRepository() *TransRepository {
+	return &TransRepository{
+		Db: configs.GetDB(),
+	}
+}
+
+func (transRpo *TransRepository) Create(trans models.Transaction) error {
+	tx := transRpo.Db.Begin()
+	err := tx.Create(&trans).Error
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	tx.Commit()
+	return err
+}
+
+func (transRpo *TransRepository) FindID(id uint) (models.Transaction, error) {
+	var trans models.Transaction
+	//SELECT * FROM transactions WHERE id = {id}
+	err := transRpo.Db.Where("id = ?", id).First(&trans).Error
+	return trans, err
+}

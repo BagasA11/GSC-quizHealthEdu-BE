6203b7c51fb5ad3cf21ec471af89837bd4de5990
unknown <111202214715@mhs.dinus.ac.id>
2024-01-31 20:21:58 +0700

    topup repository pt1

--- api/repository/topup.repository.go
+++ api/repository/topup.repository.go
@@ -1,1 +1,35 @@
 package repository
+
+import (
+	"BagasA11/GSC-quizHealthEdu-BE/api/models"
+	"BagasA11/GSC-quizHealthEdu-BE/configs"
+
+	"gorm.io/gorm"
+)
+
+type TopUpRepository struct {
+	Db *gorm.DB
+}
+
+func NewTopupRepo() *TopUpRepository {
+	return &TopUpRepository{
+		Db: configs.GetDB(),
+	}
+}
+
+func (tr *TopUpRepository) Create(topup models.TopUp) error {
+	tx := tr.Db.Begin()
+	err := tx.Create(&topup).Error
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	tx.Commit()
+	return err
+}
+
+func (tr *TopUpRepository) All() ([]models.TopUp, error) {
+	var topups []models.TopUp
+	err := tr.Db.Find(&topups).Error
+	return topups, err
+}

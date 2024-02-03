8a0163c3720161531a6cb69349731b1af2bca3ee
unknown <111202214715@mhs.dinus.ac.id>
2024-02-03 12:01:21 +0700

    update password on user routes

--- routes/user.routes.go
+++ routes/user.routes.go
@@ -5,6 +5,7 @@ package routes
 // Login for admin
 // Login for User
 // Update Username => middleware.Jwt
+// Update password => middleware.Jwt
 import (
 	"BagasA11/GSC-quizHealthEdu-BE/api/controllers"
 	"BagasA11/GSC-quizHealthEdu-BE/middleware"
@@ -19,8 +20,10 @@ func UserRoutes(group *gin.RouterGroup) {
 	group.POST("/user/register", uc.CreateUser)
 	//Create another admin
 	group.POST("/user/admin/create", middleware.JwtAuth(), uc.CreateAdmin)
-	//login user
+	//login page user
 	group.POST("/user/login", ac.UserLogin)
 	// login page for admin
 	group.POST("/user/admin/login", ac.AdmiLogin)
+	// update password
+	group.POST("/user/password", middleware.JwtAuth(), uc.UpdatePassword)
 }

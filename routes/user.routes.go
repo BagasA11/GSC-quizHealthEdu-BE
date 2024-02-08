547c46b5ed40796b14a984ffd1c39913bd262ba8
unknown <111202214715@mhs.dinus.ac.id>
2024-02-08 20:01:44 +0700

    change url for username update

--- routes/user.routes.go
+++ routes/user.routes.go
@@ -17,35 +17,35 @@ import (
 func UserRoutes(group *gin.RouterGroup) {
 	uc := controllers.NewUserController()
 	ac := controllers.NewAuthController()
+
+	//user
 	//User Registration
 	group.POST("/user/register", uc.CreateUser)
-	//Create another admin
-	group.POST("/user/admin/create", middleware.JwtAuth(), uc.CreateAdmin)
-	//login page user
+	//user login
 	group.POST("/user/login", ac.UserLogin)
-	// login page for admin
-	group.POST("/user/admin/login", ac.AdmiLogin)
-	//find user by username
+	//find user by its username
 	group.POST("/user/username", middleware.JwtAuth(), uc.FindUsername)
-	//find admin by username
-	group.POST("/user/admin/username", middleware.JwtAuth(), uc.FindAdminbyUsername)
-	//get user by id
+	//get all user
 	group.GET("/user/all", middleware.JwtAuth(), uc.AllUser)
-	//get admin by id
-	group.GET("/user/admin/:id", middleware.JwtAuth(), uc.AdminID)
-	//get all user data
-	group.GET("/user", middleware.JwtAuth(), uc.AllUser)
-	//get all type user profile
-	group.GET("/user/:id", middleware.JwtAuth(), uc.Me)
-	// update password
-	group.PUT("/user/password", middleware.JwtAuth(), uc.UpdatePassword)
+	//get user & admin profile
+	group.GET("/user", middleware.JwtAuth(), uc.Me)
 	//update username ... admin is not allowed to update username
-	group.PUT("/user/update-username", middleware.JwtAuth(), uc.UpdateUsername)
-	//block user ... admin cannot be blocked
-	group.PUT("/user/block/:id", middleware.JwtAuth(), uc.BlockUser)
-	//upload avatar image
-	group.PUT("/user/avatar", middleware.JwtAuth(), uc.UpdateAvatar)
+	group.PUT("/user/username/update", middleware.JwtAuth(), uc.UpdateUsername)
 	//delete user
 	group.DELETE("/user/delete", middleware.JwtAuth(), uc.Delete)
 
+	// ==================================
+
+	//admin
+	// login page for admin
+	group.POST("/user/admin/login", ac.AdmiLogin)
+	//Create another admin
+	group.POST("/user/admin/create", middleware.JwtAuth(), uc.CreateAdmin)
+	//find admin by username
+	group.POST("/user/admin/username", middleware.JwtAuth(), uc.FindAdminbyUsername)
+	//get admin by id
+	group.GET("/user/admin/:id", middleware.JwtAuth(), uc.AdminID)
+	//block user ... admin cannot be blocked
+	group.PUT("/user/block/:id", middleware.JwtAuth(), uc.BlockUser)
+	//general
 }
